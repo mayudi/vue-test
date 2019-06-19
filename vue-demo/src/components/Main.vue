bd7cb92be05ab387d60708f900a36f9600b0bcf5
mayudi <mayudi@flashhold.com>
2019-06-19 18:48:36 +0800

    初始化

---
+++ vue-demo/src/components/Main.vue
@@ -0,0 +1,91 @@
+<template>
+  <div class="manage_page fillcontain">
+    <el-row style="height: 100%;">
+      <el-col :span="4"   style="min-height: 100%; background-color: #324057;">
+        <el-menu
+          default-active="2"
+          style="min-height: 100%;" theme="dark" router>
+          <el-submenu index="1">
+            <template slot="title">
+              <i class="el-icon-location"></i>
+              <span>导航一</span>
+            </template>
+            <el-menu-item-group>
+              <template slot="title">分组一</template>
+              <el-menu-item index="1-1">选项1</el-menu-item>
+              <el-menu-item index="1-2">选项2</el-menu-item>
+            </el-menu-item-group>
+            <el-menu-item-group title="分组2">
+              <el-menu-item index="1-3">选项3</el-menu-item>
+            </el-menu-item-group>
+            <el-submenu index="1-4">
+              <template slot="title">选项4</template>
+              <el-menu-item index="1-4-1">选项1</el-menu-item>
+            </el-submenu>
+          </el-submenu>
+          <el-menu-item index="2">
+            <i class="el-icon-menu"></i>
+            <span slot="title">导航二</span>
+          </el-menu-item>
+          <el-menu-item index="3" disabled>
+            <i class="el-icon-document"></i>
+            <span slot="title">导航三</span>
+          </el-menu-item>
+          <el-menu-item index="4">
+            <i class="el-icon-setting"></i>
+            <span slot="title">导航四</span>
+          </el-menu-item>
+        </el-menu>
+      </el-col>
+
+      <el-col :span="20" style="height: 100%;overflow: auto;">
+        <div class="header_container">
+
+          <el-breadcrumb separator="/">
+            <el-breadcrumb-item :to="{ path: '/manage' }">首页</el-breadcrumb-item>
+            <el-breadcrumb-item v-for="(item, index) in $route.meta" :key="index">{{item}}</el-breadcrumb-item>
+          </el-breadcrumb>
+          <el-dropdown menu-align='start'>
+            <img class="avator">
+            <el-dropdown-menu slot="dropdown">
+              <el-dropdown-item command="home">首页</el-dropdown-item>
+              <el-dropdown-item command="signout">退出</el-dropdown-item>
+            </el-dropdown-menu>
+          </el-dropdown>
+        </div>
+      </el-col>
+    </el-row>
+  </div>
+</template>
+
+<script>
+  export default {
+    computed: {
+      defaultActive: function () {
+        return this.$route.path.replace('/', '')
+      }
+    }
+  }
+</script>
+<style scoped lang="less">
+  @import '../style/mixin';
+
+  .header_container {
+    background-color: #EFF2F7;
+    height: 60px;
+    display: flex;
+    //  justify-content: space-between;
+    align-items: center;
+    padding-left: 20px;
+  }
+
+  .avator {
+    .wh(36px, 36px);
+    border-radius: 50%;
+    margin-right: 37px;
+  }
+
+  .el-dropdown-menu__item {
+    text-align: center;
+  }
+</style>
